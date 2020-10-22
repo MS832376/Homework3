e7acd4bc240e1bb132dba055b2d1aa285f15a3d3
MS832376 <MS832376@wcupa.edu>
2020-10-21 22:03:09 -0400

    Deleting Non Essentials
    Deleted a few scripts that were no longer being used.
    Transfered ShotScores uses directly to MissionDemolition.cs
    Deleted "Winner" scene because there was no real point.
    Deleted FinalScreen.cs because it was only used on winner scene

--- Assets/_Scripts/MissionDemolition.cs
+++ Assets/_Scripts/MissionDemolition.cs
@@ -17,14 +17,14 @@ public class MissionDemolition : MonoBehaviour
     public Text uitLevel;
     public Text uitShots;
     public Text uitButton;
+    public Text uitBest;
     public Vector3 castlePos;
     public GameObject[] castles;
     [Header("Set Dynamically")]
-    public static int level;
-    public static int levelMax;
-    public static int totalShots;
-    public static int prevBest;
+    public int level;
+    public int levelMax;
     public int shotsTaken;
+    public int[] score;
     public GameObject castle;
     public GameMode mode = GameMode.idle;
     public string showing = "Show Slingshot";
@@ -33,9 +33,8 @@ public class MissionDemolition : MonoBehaviour
     {
         S = this;
         level = 0;
-        totalShots = 0;
-        prevBest = ShotScores.score;
         levelMax = castles.Length;
+        score = new int[levelMax];
         StartLevel();
     }
     void StartLevel(){
@@ -46,10 +45,15 @@ public class MissionDemolition : MonoBehaviour
         foreach(GameObject pTemp in gos){
             Destroy(pTemp);
         }
+        
         castle = Instantiate<GameObject>(castles[level]);
         castle.transform.position = castlePos;
         shotsTaken = 0;
-
+        if(PlayerPrefs.HasKey("HighScore_"+level)){
+            score[level] = PlayerPrefs.GetInt("HighScore_"+level);
+        }else{
+            score[level] = 10;
+        }
         SwitchView("Show Both");
         ProjectileLine.S.Clear();
         Goal.goalMet = false;
@@ -59,13 +63,15 @@ public class MissionDemolition : MonoBehaviour
     void UpdateGUI(){
         uitLevel.text = "Level: " +(level+1)+" of " + levelMax;
         uitShots.text = "Shots Taken: " + shotsTaken;
+        uitBest.text = "Best Score: " + score[level];
     }
     void Update(){
         UpdateGUI();
         if((mode == GameMode.playing) && Goal.goalMet){
-            //if(shotsTaken < ShotScores.score){
-            //    ShotScores.score = shotsTaken;
-            //}
+            if(shotsTaken < score[level]){
+                score[level] = shotsTaken;
+                PlayerPrefs.SetInt("HighScore_"+level, score[level]);
+            }
             mode = GameMode.levelEnd;
             SwitchView("Show Both");
             Invoke("NextLevel",2f);
@@ -73,13 +79,15 @@ public class MissionDemolition : MonoBehaviour
     }
     void NextLevel(){
         level++;
-        totalShots = totalShots + shotsTaken;
         if(level == levelMax){
-            if(totalShots < ShotScores.score){
-                ShotScores.score = totalShots;
-            }
-            SceneManager.LoadScene("Winner");
+            level = 0;
         }
+        if(PlayerPrefs.HasKey("HighScore_"+level)){
+            score[level] = PlayerPrefs.GetInt("HighScore_"+level);
+        }else{
+            score[level] = 10;
+        }
+        
         StartLevel();
     }
     public void RestartLevel(){
